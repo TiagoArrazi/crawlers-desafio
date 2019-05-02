b13dec54f5362f1896b4051f953c982055cea9e3
Tiago Arrazi <tiago_arrazi98@outlook.com>
2019-05-02 10:50:41 -0300

    crawlers para o desafio

---
+++ crypto_crawler.py
@@ -0,0 +1,26 @@
+from os import stat
+from bs4 import BeautifulSoup
+from csv import writer
+from datetime import datetime
+from requests import ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError, get
+
+crypto_Url = "https://m.investing.com/crypto/"
+
+requestString = get(url = crypto_Url, headers = {'User-Agent':'curl/7.52.1'})
+soup = BeautifulSoup(requestString.text, "lxml")
+content = soup.findAll('tr')
+date = datetime.strptime(requestString.headers['Date'][:-4], '%a, %d %b %Y %H:%M:%S')
+
+with open("crypto.csv", "a+") as f:
+
+        w = writer(f, delimiter = ";")
+
+        if stat("Registro.csv").st_size == 0:
+            w.writerow(['code', 'name', 'priceUSD', 'change24H', 'change7D', 'symbol', 'priceBTC', 'marketCap', 'volume24H', 'totalVolume', 'timestamp'])
+
+        for c in content[1:]:
+
+            l = c.get_text().replace('\t', '').split('\n')
+            L = list(filter(None, l))
+
+
---
+++ dolar_crawler.py
@@ -0,0 +1,63 @@
+from bs4 import BeautifulSoup
+from requests import ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError, get
+import sys
+import csv
+import os
+import datetime
+
+def beautifulValueScraper(soup):
+
+    tag = soup.find("span", class_ = "pid-2103-last")
+    return tag.get_text().strip()
+
+
+def beautifulCurrencyScraper(soup):
+
+    tag = soup.find("h1", class_ = "instrumentH1inlineblock")
+    return tag.get_text().strip()
+
+
+def beautifulVariationScraper(soup):
+
+    tagVar = soup.find("i", class_ = "pid-2103-pc")
+    return tagVar.get_text().strip()
+
+
+def beautifulPercentageScraper(soup):
+
+    tagPerc = soup.find("i", class_ = "pid-2103-pcp")
+    return tagPerc.get_text().strip()
+
+
+def getTimestamp(requestString):
+
+    date = requestString.headers["Date"][:-4]
+    timestamp = datetime.datetime.strptime(date, "%a, %d %b %Y %H:%M:%S")
+    return timestamp
+
+
+if __name__ == '__main__':
+
+    url = 'https://m.investing.com/currencies/usd-brl'
+    filename = 'dolar.csv'
+
+    try:
+        requestString = get(url = url, headers = {'User-Agent':'curl/7.52.1'})
+        soup = BeautifulSoup(requestString.text, "html.parser")
+
+        with open('dolar.csv', 'a+') as f:
+            w = csv.writer(f, delimiter=';')
+
+            if os.stat(filename).st_size == 0:
+                w.writerow(['currency', 'value', 'change', 'perc', 'timestamp'])
+
+            w.writerow([beautifulCurrencyScraper(soup), beautifulValueScraper(soup), 
+                        beautifulVariationScraper(soup), beautifulPercentageScraper(soup), 
+                        getTimestamp(requestString)])
+
+    except (ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError):
+        print("Couldn't connect, quitting!")
+        sys.exit()
+
+
+
