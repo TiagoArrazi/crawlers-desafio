9d7d031ea571b5ad676c485f9eb9c65b25019a4a
Tiago Arrazi <tiago_arrazi98@outlook.com>
2019-05-10 01:44:34 -0300

    sparker added

---
+++ sparker.py
@@ -0,0 +1,18 @@
+from subprocess import Popen
+from datetime import datetime
+
+
+def get_current_timestamp():
+
+    return ''.join(['0' + str(t)
+                     if t in list(range(1,10))
+                     else str(t)
+                     for t in list(datetime.now().timetuple())[:-6]])
+
+
+
+dolar_filename = '/user/tiagoArrazi/dolar_{}.csv'.format(get_current_timestamp())
+crypto_filename = '/user/tiagoArrazi/crypto_{}.csv'.format(get_current_timestamp())
+jar_file = 'spark_process_2.11-0.1.0-SNAPSHOT.jar'
+
+Popen("spark-submit {} {} {}".format(jar_file, crypto_filename, dolar_filename), shell=True).communicate()
