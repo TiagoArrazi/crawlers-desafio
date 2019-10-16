f05a87b5fbac16edbe55cf5445a599cd80b76f7d
Tiago Arrazi <tiago_arrazi98@outlook.com>
2019-10-16 19:59:37 -0300

    Commit through GitPusher

---
+++ consolidate_daily_crypto.py
@@ -0,0 +1,13 @@
+import glob
+import subprocess
+
+from csv import DictReader, writer
+from os import chdir
+
+
+
+subprocess.call('pwd', shell=True)
+chdir('/home')
+subprocess.call('pwd', shell=True)
+
+    
