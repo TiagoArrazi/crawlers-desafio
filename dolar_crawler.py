c0d75c05a4dec199a549704ebf214bc83208f6c4
Tiago Arrazi <tiago_arrazi98@outlook.com>
2019-10-18 08:45:38 -0300

    Commit through GitPusher

--- dolar_crawler.py
+++ dolar_crawler.py
@@ -6,53 +6,49 @@ import os
 import datetime
 from subprocess import Popen, PIPE
 
-def beautifulValueScraper(soup):
+def value(soup):
 
     tag = soup.find("span", class_ = "pid-2103-last")
     return tag.get_text().strip()
 
 
-def beautifulCurrencyScraper(soup):
+def currency(soup):
 
     tag = soup.find("h1", class_ = "instrumentH1inlineblock")
     return tag.get_text().strip()
 
 
-def beautifulVariationScraper(soup):
+def variation(soup):
 
     tagVar = soup.find("i", class_ = "pid-2103-pc")
     return tagVar.get_text().strip()
 
 
-def beautifulPercentageScraper(soup):
+def percentage(soup):
 
     tagPerc = soup.find("i", class_ = "pid-2103-pcp")
     return tagPerc.get_text().strip()
 
 
-def getTimestamp(requestString):
+def get_timestamp(requestString):
 
     date = requestString.headers["Date"][:-4]
-    timestamp = ''.join(['0' + str(t) 
-                          if t in list(range(1,10)) 
-                          else str(t) 
-                          for t in list(datetime.datetime.strptime(date, "%a, %d %b %Y %H:%M:%S").timetuple())[:-6]])
+    timestamp = ''.join([str(t).zfill(2)
+                         for t in list(datetime.datetime.strptime(date, "%a, %d %b %Y %H:%M:%S").timetuple())[:-6]])
     return timestamp
 
 
-def getCurrentTimestamp():
+def get_curr_timestamp():
 
-    return ''.join(['0' + str(t) 
-                     if t in list(range(1,10)) 
-                     else str(t) 
-                     for t in list(datetime.datetime.now().timetuple())[:-6]])
+    return ''.join([str(t).zfill(2)
+                    for t in list(datetime.datetime.now().timetuple())[:-6]])
 
 
 if __name__ == '__main__':
 
     url = 'https://m.investing.com/currencies/usd-brl'
     os.chdir('~/tiagoArrazi/crawler_dolar')
-    filename = "dolar_{}.csv".format(getCurrentTimestamp())
+    filename = "dolar_{}.csv".format(get_curr_timestamp())
 
     try:
         requestString = get(url=url, headers={'User-Agent':'curl/7.52.1'})
@@ -64,9 +60,9 @@ if __name__ == '__main__':
             if os.stat(filename).st_size == 0:
                 w.writerow(['currency', 'value', 'change', 'perc', 'timestamp'])
 
-            w.writerow([beautifulCurrencyScraper(soup), beautifulValueScraper(soup), 
-                        beautifulVariationScraper(soup), beautifulPercentageScraper(soup), 
-                        getTimestamp(requestString)])
+            w.writerow([currency(soup), value(soup), 
+                        variation(soup), percentage(soup), 
+                        get_timestamp(requestString)])
 
     except (ReadTimeout, ConnectTimeout, HTTPError, Timeout, ConnectionError):
         print("Couldn't connect, quitting!")
